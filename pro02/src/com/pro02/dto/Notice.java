abec51efd4af88c7baa385a7b69e3e7eff9c01c2
dksmsdlskgus <lnh03280@g.eulji.ac.kr>
2023-10-28 16:46:28 +0900

    notice 작성

--- pro02/src/com/pro02/dto/Notice.java
+++ pro02/src/com/pro02/dto/Notice.java
@@ -1,4 +1,60 @@
 package com.pro02.dto;
 
 public class Notice {
+    private int nno;
+    private String title;
+    private String content;
+    private String regdate;
+    private int visit;
+
+    public int getNno() {
+        return nno;
+    }
+
+    public void setNno(int nno) {
+        this.nno = nno;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getRegdate() {
+        return regdate;
+    }
+
+    public void setRegdate(String regdate) {
+        this.regdate = regdate;
+    }
+
+    public int getVisit() {
+        return visit;
+    }
+
+    public void setVisit(int visit) {
+        this.visit = visit;
+    }
+
+    @Override
+    public String toString() {
+        return "Notice{" +
+                "nno=" + nno +
+                ", title='" + title + '\'' +
+                ", content='" + content + '\'' +
+                ", regdate='" + regdate + '\'' +
+                ", visit=" + visit +
+                '}';
+    }
 }
