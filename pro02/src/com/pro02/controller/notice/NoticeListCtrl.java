9e32a7bedf38809765e4a9ad97368c590bc71b2f
dksmsdlskgus <lnh03280@g.eulji.ac.kr>
2023-10-28 16:19:00 +0900

    notice 기록

---
+++ pro02/src/com/pro02/controller/notice/NoticeListCtrl.java
@@ -0,0 +1,21 @@
+package com.pro02.controller.notice;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/NoticeListCtrl")
+public class NoticeListCtrl extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+
+    }
+}
