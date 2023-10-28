b4dc627f02d7f550ba35e27562ff4a42b9cf6e73
dksmsdlskgus <lnh03280@g.eulji.ac.kr>
2023-10-28 16:45:55 +0900

    custom 작성

--- pro02/src/com/pro02/dto/Custom.java
+++ pro02/src/com/pro02/dto/Custom.java
@@ -1,4 +1,120 @@
 package com.pro02.dto;
 
 public class Custom {
+    private String id;
+    private String pw;
+    private String name;
+    private String email;
+    private String tel;
+    private String add1;
+    private String add2;
+    private String postcode;
+    private String birth;
+    private int pt;
+    private int visited;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getPw() {
+        return pw;
+    }
+
+    public void setPw(String pw) {
+        this.pw = pw;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTel() {
+        return tel;
+    }
+
+    public void setTel(String tel) {
+        this.tel = tel;
+    }
+
+    public String getAdd1() {
+        return add1;
+    }
+
+    public void setAdd1(String add1) {
+        this.add1 = add1;
+    }
+
+    public String getAdd2() {
+        return add2;
+    }
+
+    public void setAdd2(String add2) {
+        this.add2 = add2;
+    }
+
+    public String getPostcode() {
+        return postcode;
+    }
+
+    public void setPostcode(String postcode) {
+        this.postcode = postcode;
+    }
+
+    public String getBirth() {
+        return birth;
+    }
+
+    public void setBirth(String birth) {
+        this.birth = birth;
+    }
+
+    public int getPt() {
+        return pt;
+    }
+
+    public void setPt(int pt) {
+        this.pt = pt;
+    }
+
+    public int getVisited() {
+        return visited;
+    }
+
+    public void setVisited(int visited) {
+        this.visited = visited;
+    }
+
+    @Override
+    public String toString() {
+        return "Custom{" +
+                "id='" + id + '\'' +
+                ", pw='" + pw + '\'' +
+                ", name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                ", tel='" + tel + '\'' +
+                ", add1='" + add1 + '\'' +
+                ", add2='" + add2 + '\'' +
+                ", postcode='" + postcode + '\'' +
+                ", birth='" + birth + '\'' +
+                ", pt=" + pt +
+                ", visited=" + visited +
+                '}';
+    }
 }
